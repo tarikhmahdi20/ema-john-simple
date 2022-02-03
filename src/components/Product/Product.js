e2d04563c27796b856911a61693cb4d0bc4d9684
unknown <tarikh.pf@gmail.com>
2022-02-03 11:52:04 +0600

    Add cart button

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product);
+    //console.log(props);
     const { img, name, seller, price, stock } = props.product;
     return (
         <div className='product'>
@@ -16,6 +18,7 @@ const Product = (props) => {
                 <p>${price}</p>
                 <br />
                 <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button className='main-button'onClick={()=>props.handleAddProduct(props.product)}> <FontAwesomeIcon icon={faShoppingCart}/>add to cart</button>
             </div>
 
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,18 +6,28 @@ import './Shop.css'
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const[products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct =(product)=>{
+        console.log("product Added",product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     
     return (
         <div className="shop-container">
             <div className="product-container">
            
             {
-                products.map(product=><Product product={product}></Product>)
+                products.map(product=><Product 
+                    handleAddProduct ={handleAddProduct}
+                    product={product}></Product>)
             }
         
             </div>
             <div className="card-container">
                 <h3>This is Card</h3>
+                <h5>Order Summary : {cart.length}</h5>
             </div>
             
             
